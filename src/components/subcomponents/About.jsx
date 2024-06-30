0b1fd1ddc789c1d3c11b43fecf2f16358846e75f
ZIONIB247 <ZIONIB247>
2024-06-30 13:01:34 +0100

    updated branch

--- src/components/subcomponents/About.jsx
+++ src/components/subcomponents/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
         <h1><span>abo</span>ut Us</h1>
 
         <p>
-         our aim as a foundation is to preach the Gospel of our lord Jesus Christ all over the 
+         Our aim as a foundation is to preach the Gospel of our lord Jesus Christ all over the 
          world. and also to  establish a seed of love that will promote mutual understanding resulting
          to peace and unity among men and brethren. and to assist the less privilaged in the society..
          we also collaborate with other NGO ,organisations, associations , churches and other outreach
